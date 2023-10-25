be0bd0d59dbdeb6ecdc11a39d998ed6c83aa637d
Андрей Цай <skaeris@yandex.ru>
2023-10-25 15:08:12 +0300

    Second  comment after rewiew_4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,11 +7,17 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', '123')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['130.193.41.14', '127.0.0.1', 'localhost', 'kittyinfragramm.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
+
+# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+
+# DEBUG = True
+
+# ALLOWED_HOSTS = ['130.193.41.14', '127.0.0.1', 'localhost', 'kittyinfragramm.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
